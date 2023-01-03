867c821fe85dca7b131e16452e7190d6ea1dc666
Ray Graham <rayg@bitwisedesign.com>
2023-01-03 11:17:41 -0800

    HB-4908: Update adapters to use HeliumErrorCode (#7)
    * HB-4908: Update adapters to use HeliumErrorCode
    
    * Apply suggestions from code review

--- Source/IronSourceAdapterRewardedAd.swift
+++ Source/IronSourceAdapterRewardedAd.swift
@@ -38,7 +38,7 @@ final class IronSourceAdapterRewardedAd: IronSourceAdapterAd, PartnerAd {
         
         // If ad not loaded fail immediately
         guard IronSource.hasISDemandOnlyRewardedVideo(request.partnerPlacement) else {
-            let error = error(.noAdReadyToShow)
+            let error = error(.showFailureAdNotReady)
             log(.showFailed(error))
             completion(.failure(error))
             return
@@ -63,7 +63,7 @@ extension IronSourceAdapterRewardedAd: CHBHIronSourceWrapperRewardedDelegate {
     
     func rewardedVideoDidFailToLoadWithError(_ partnerError: Error, instanceId: String) {
         // Report load failure
-        let error = error(.loadFailure, error: partnerError)
+        let error = error(.loadFailureUnknown, error: partnerError)
         log(.loadFailed(error))
         loadCompletion?(.failure(error)) ?? log(.loadResultIgnored)
         loadCompletion = nil
@@ -78,7 +78,7 @@ extension IronSourceAdapterRewardedAd: CHBHIronSourceWrapperRewardedDelegate {
     
     func rewardedVideoDidFailToShowWithError(_ partnerError: Error, instanceId: String) {
         // Report show failure
-        let error = error(.showFailure, error: partnerError)
+        let error = error(.showFailureUnknown, error: partnerError)
         log(.showFailed(error))
         showCompletion?(.failure(error)) ?? log(.showResultIgnored)
         showCompletion = nil
