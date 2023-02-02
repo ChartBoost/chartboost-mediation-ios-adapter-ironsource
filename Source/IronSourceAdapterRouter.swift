43bf18ad3ff04ca0316cc15085c3263f74802093
Alex Rice <rice.alexanderb@gmail.com>
2023-02-02 09:53:59 -0800

    Remove extra lines from file headers

--- Source/IronSourceAdapterRouter.swift
+++ Source/IronSourceAdapterRouter.swift
@@ -1,14 +1,7 @@
 // Copyright 2022-2023 Chartboost, Inc.
-// 
+//
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
-
-//
-//  IronSourceAdapterRouter.swift
-//  ChartboostMediationAdapterIronSource
-//
-//  Created by Daniel Barros on 11/21/22.
-//
 
 import ChartboostMediationSDK
 import Foundation
