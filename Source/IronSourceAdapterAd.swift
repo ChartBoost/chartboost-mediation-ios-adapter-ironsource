577f1309f082b5fab6dbb783ff2fa824b71e99dc
Daniel Barros <daniel.barros@chartboost.com>
2024-04-17 13:14:35 +0530

    Split makeAd functions

--- Source/IronSourceAdapterAd.swift
+++ Source/IronSourceAdapterAd.swift
@@ -17,11 +17,11 @@ class IronSourceAdapterAd: NSObject {
     var details: PartnerDetails = [:]
 
     /// The ad load request associated to the ad.
-    /// It should be the one provided on `PartnerAdapter.makeAd(request:delegate:)`.
+    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)`` or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
     let request: PartnerAdLoadRequest
     
     /// The partner ad delegate to send ad life-cycle events to.
-    /// It should be the one provided on `PartnerAdapter.makeAd(request:delegate:)`.
+    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)`` or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
     weak var delegate: PartnerAdDelegate?
     
     /// The partner ad view to display inline. E.g. a banner view.
